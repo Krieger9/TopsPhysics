50d1be5cbd3d45b30961136307fa2d07e407faed
George Hassan <ghassan_iii@hotmail.com>
2019-01-26 19:25:35 -0500

    Initial Checkin

---
+++ Physics/TopsPhysics/Assets/Bowl/Scripts/DuplicateAround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicateAround : MonoBehaviour {
+    public float PieceCount;
+    public GameObject PieceToRotate;
+    public List<GameObject> createdObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        DoDuplication360();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void DoDuplication360()
+    {
+        var rotationDegrees = 360f / PieceCount;
+        var startLocation = PieceToRotate.transform.position;
+        var startRotation = PieceToRotate.transform.rotation;
+        var WallCollidersParent = transform.Find("Bowl/WallColliders");
+
+        for(float counter = 0; counter < PieceCount; counter++)
+        {
+            var newPiece = Instantiate<GameObject>(PieceToRotate,WallCollidersParent.transform);
+            newPiece.transform.position = Quaternion.Euler(0, rotationDegrees * counter, 0) * startLocation;
+            newPiece.transform.rotation = startRotation;
+            newPiece.transform.Rotate(-this.transform.up, rotationDegrees * counter,Space.World);
+            newPiece.SetActive(true);
+        }
+    }
+}
---
+++ Physics/TopsPhysics/Assets/Tops/Scripts/Spin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+    public bool ReverseDirection = false;
+
+    private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = this.GetComponent<Rigidbody>();
+        rigidBody.maxAngularVelocity = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidBody.AddRelativeTorque(Vector3.forward * 20000 * (ReverseDirection ? 1 : -1));
+	}
+}
