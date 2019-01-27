0a1fd5b6797a0126802ca35373d5a69e5e33b1cd
George Hassan <ghassan_iii@hotmail.com>
2019-01-27 12:26:10 -0500

    Adding Library constants

---
+++ Physics/TopsPhysics/Assets/Library/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants {
+    public static readonly string BowlArena = "BowlArena";
+    public static readonly string PlayerTop = "PlayerTop";
+    public static readonly string Environment = "Environment";
+}
