a19088ab738e5a7093b38ee33b26463e26ee2302
George Hassan <ghassan_iii@hotmail.com>
2019-02-04 17:47:06 -0500

    Tweaking controls, little camera follow

---
+++ Physics/TopsPhysics/Assets/FollowObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    public GameObject ObjectToFollow;
+    public float maxDistance;
+    public float minDistance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var differenceVector = ObjectToFollow.transform.position - this.transform.position;
+
+        if (differenceVector.magnitude > maxDistance)
+        {
+            this.transform.position = ObjectToFollow.transform.position - (differenceVector.normalized * maxDistance);
+        }
+        else if(differenceVector.magnitude < minDistance)
+        {
+            this.transform.position = ObjectToFollow.transform.position - (differenceVector.normalized * minDistance);
+        }
+        this.transform.LookAt(ObjectToFollow.transform);
+	}
+}
